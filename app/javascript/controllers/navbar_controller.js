ec2a7dad9b4e577f087b8a668aa75291c0a62d2f
Thanh Tran <trandgthanh@gmail.com>
2021-11-11 10:42:07 +0100

    done lecture

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  // connect() {
+  //   console.log("hello from navbar_controller!");
+  // }
+
+  updateNavbar() {
+    // console.log(window.scrollY)
+    // console.log(window.innerHeight)
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add('navbar-lewagon-white')
+    } else {
+      this.element.classList.remove('navbar-lewagon-white')
+    }
+  }
+}
